50c83037ec1c6fb09858105b831ca91fff20be10
Ákos <akosmagyar17@gmail.com>
2019-04-25 20:07:23 +0200

    adding some style

--- prf-projekt/src/app/app.module.ts
+++ prf-projekt/src/app/app.module.ts
@@ -2,16 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // !!
-import { MatInputModule, MatCardModule, MatButtonModule} from '@angular/material'
+import {
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
+   } from '@angular/material'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component'; // automatikusan hozzáadta, de figyelni rá
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +30,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserAnimationsModule, // hozzá kell adni az importokhoz is, 2 way binding
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
